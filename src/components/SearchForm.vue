a3ef47f84fb89629a922ea6f61dfb2b09a597380
pedro-donoso <pedro.donoso.r@protonmail.com>
2020-08-17 17:08:39 -0400

    Agrego componente

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="form-group mb-2">
+        <label class="sr-only">Escribe tu búsqueda</label>
+        <input type="text" v-model="search" class="form-control" />
+      </div>
+
+      <div class="form-check form-check-inline">
+        <input
+          class="form-radio-input"
+          v-model="available"
+          :value="true"
+          type="radio"
+        />
+
+        Disponible
+      </div>
+
+      <div class="form-check form-check-inline">
+        <input
+          class="form-radio-input"
+          v-model="available"
+          :value="false"
+          type="radio"
+        />
+
+        No disponible
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
