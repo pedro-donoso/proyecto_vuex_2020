2253c8999c8ce047a079b103e83301a60f5f56cb
pedro-donoso <pedro.donoso.r@protonmail.com>
2020-08-17 22:41:21 -0400

    afrego mapgetters en componente  MovieList

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-sm-6 mb-3">
+    <div class="card">
+      <div class="card-header">
+        <h5 class="card-title">{{ movie.title }}</h5>
+        <h6 class="card-subtitle">Available: {{ movie.available }}</h6>
+      </div>
+      <div class="card-body">
+        <p class="card-text">{{ movie.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        movie: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
+
+<style></style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+  
+  </div>
+</template>
+
+<script>
+
+import Movie from './MovieItem';
+import {mapGetters} from 'vuex';
+
+export default {
+    components: {
+        Movie
+    },
+
+    computed: {
+        ...mapGetters({
+            movies: 'filteredMovies',
+        })
+    }
+}
+
+</script>
+
+<style>
+
+</style>
